bede59350f97a09e734cfce97cc94fb1d1d30232
Leandro Martins <lmarttinssilva@gmail.com>
2017-08-21 19:33:53 -0300

    remove dollar and franc

--- src/Tests/MoneyTest.php
+++ src/Tests/MoneyTest.php
@@ -4,7 +4,6 @@ namespace Money\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-use Money\Franc;
 use Money\Money;
 
 class MoneyTest extends TestCase
@@ -23,23 +22,9 @@ class MoneyTest extends TestCase
         $this->assertFalse(Money::franc(5)->equals(Money::dollar(5)));
     }
 
-    public function testFrancMultiplication()
-    {
-        $five = Money::franc(5);
-        $this->assertEquals(Money::franc(10), $five->times(2));
-        $this->assertEquals(Money::franc(15), $five->times(3));
-    }
-
     public function testCurrency()
     {
         $this->assertEquals('USD', Money::dollar(1)->currency());
         $this->assertEquals('CHF', Money::franc(1)->currency());
-    }
-
-    public function testDifferentClassEquality()
-    {
-        $this->assertTrue(
-            (new Money(10, 'CHF'))->equals(new Franc(10, 'CHF'))
-        );
     }
 }
